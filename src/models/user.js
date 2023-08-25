f67fbfe8d39563559af920b19739278b97856dfc
Yogesh-Mujawdiya <sweyogesgkumar@gmail.com>
2023-08-25 17:14:06 +0530

    update

--- src/models/user.js
+++ src/models/user.js
@@ -24,6 +24,11 @@ const userSchema = new mongoose.Schema({
         required: true,
         lowercase: true,
         unique: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid Email')
+            }
+        }
     },
 })
 
